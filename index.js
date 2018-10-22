e7493ae91aabc634dee4b9fd546ac10e15e97ab3
Chris Williamson <chris.williamson@bbc.co.uk>
2018-10-22 20:10:18 +0100

    Edited index to do budgeting operations.

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
+const config = require('config');
 const AWS = require('aws-sdk');
 
 const Client = require('./src/api/client');
 const getSecret = require('./src/apiAccess/getSecret');
-// const listPots = require('./src/api/listPots');
-const getGrossSpend = require('./src/api/getGrossSpend');
-const getGrossReceived = require('./src/api/getGrossReceived');
-const listTransactions = require('./src/api/listTransactions');
 const getBudgetingRange = require('./src/utils/date/getBudgetingRange');
 const getBudgetingPot = require('./src/api/getBudgetingPot');
+const potTransaction = require('./src/api/potTransaction');
+const getNetSpend = require('./src/api/getNetSpend');
+const toPounds = require('./src/utils/toPounds');
 
 const endpoint = 'https://secretsmanager.eu-west-2.amazonaws.com';
 const region = 'eu-west-2';
@@ -21,10 +21,21 @@ const secretsClient = new AWS.SecretsManager({ region, endpoint });
       await getSecret(secretsClient, 'accessToken')
     );
     monzoClient.setBudgetingPot(await getBudgetingPot(monzoClient));
-    console.log(await listTransactions(monzoClient, getBudgetingRange()));
-    console.log(await getGrossSpend.inDateRange(monzoClient, getBudgetingRange()));
-    console.log(await getGrossReceived.inDateRange(monzoClient, getBudgetingRange()));
-
+    const netSpendYesterday = await getNetSpend(monzoClient, getBudgetingRange());
+    const budgetInPence = config.get('dailyBudget') * 100;
+    const saved = budgetInPence - netSpendYesterday
+    if (saved > 0) {
+      console.log(`You have saved £${toPounds(saved)}. Depositing into budgeting pot...`);
+      potTransaction.deposit(monzoClient, monzoClient.budgetingPot.id, saved);
+    } else if (saved == 0) {
+      console.log(`You have broke even today.`);
+      return;
+    } else {
+      console.log(`You have gone over budget by £${toPounds(saved)}. Withdrawing from budgeting pot...`);
+      potTransaction.withdraw(monzoClient, monzoClient.budgetingPot.id, saved);
+    }
+    
+    console.log(`Transaction complete`);
   } catch (e) {
     console.log(e)
   }

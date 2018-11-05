d35190e702c74e5984a07c80f1c025cf7c8d9272
Chris Williamson <chris.williamson@bbc.co.uk>
2018-11-05 08:55:25 +0000

    Fixed bug in index where it would try to set overspend to a negative number. Also added some logs.:

--- src/handleBudgetingPot.js
+++ src/handleBudgetingPot.js
@@ -6,11 +6,13 @@ module.exports = async (amountSaved, monzoClient, secretsClient) => {
     await potTransaction.deposit(monzoClient, monzoClient.budgetingPot.id, amountSaved);
   } else {
     const amountOverspent = Math.abs(amountSaved);
-
+    console.log(`Overspent by ${amountOverspent}`);
     if (amountOverspent > monzoClient.budgetingPot.balance) {
-      const outstandingOverspend = monzoClient.budgetingPot.balance - amountOverspent;
+      const outstandingOverspend = Math.abs(monzoClient.budgetingPot.balance - amountOverspent);
       await potTransaction.withdraw(monzoClient, monzoClient.budgetingPot.id, monzoClient.budgetingPot.balance);
+      console.log(`Emptied budgeting pot (${monzoClient.budgetingPot.balance})`);
       await setOverspendAmount(outstandingOverspend, secretsClient);
+      console.log(`Set total overspend to ${outstandingOverspend}`);
     } else {
       await potTransaction.withdraw(monzoClient, monzoClient.budgetingPot.id, amountOverspent);
     }
--- src/handleMonthlyOverspend.js
+++ src/handleMonthlyOverspend.js
@@ -3,6 +3,7 @@ const setOverspendAmount = require('./api/setOverspendAmount')
 module.exports = async (secretsClient, saved, overspend) => {
   const newOverspendAmount = overspend - saved;
   await setOverspendAmount(newOverspendAmount, secretsClient);
+  console.log(`Set new overspend amount: ${newOverspendAmount}`);
   if (newOverspendAmount > 0) {
     return 0;
   }
